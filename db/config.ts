291e01b31aa9c3a8608b7eee05ac5992fd5a0aa6
Lance Turbes <instinct_truster@protonmail.com>
2024-07-11 16:20:34 -0700

    end of day

---
+++ db/config.ts
@@ -0,0 +1,14 @@
+import { defineDb, defineTable, column, NOW } from "astro:db";
+
+const GuestBook = defineTable({
+  columns: {
+    id: column.number({ primaryKey: true }),
+    author: column.text(),
+    content: column.text(),
+    timestamp: column.date({ default: NOW }),
+  },
+});
+
+export default defineDb({
+  tables: {},
+});
